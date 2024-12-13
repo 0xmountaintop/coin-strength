060c0102fb49f45cf3cba27a4aa408f5f9f6a4eb
0xmountaintop <haoyu@protonmail.com>
2024-12-13 14:59:29 +1100

    update comments

--- src/config.ts
+++ src/config.ts
@@ -6,9 +6,9 @@ const COIN_LIST_FILE = 'coin_list.txt';
 const DATE = process.env.DATE || new Date().toISOString().split('T')[0];
 
 const periods: Period[] = [
-  { start: new Date('2024-08-04'), end: new Date('2024-08-06') },
-  { start: new Date('2024-09-06'), end: new Date('2024-09-08') },
-  { start: new Date('2024-11-05'), end: new Date('2024-11-07') },
+  { start: new Date('2024-08-04'), end: new Date('2024-08-06') }, // BTC dip
+  { start: new Date('2024-09-06'), end: new Date('2024-09-08') }, // BTC dip
+  { start: new Date('2024-11-05'), end: new Date('2024-11-07') }, // trump win
 ];
 
 export { PRICE_DATA_FILE, COIN_LIST_FILE, periods, DATE };
