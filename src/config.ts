5c962d655cdae164a15647dd9388a581689dd39b
0xmountaintop <haoyu@protonmail.com>
2024-12-14 14:09:53 +1100

    clean up

--- src/config.ts
+++ src/config.ts
@@ -11,10 +11,4 @@ const periods: Period[] = [
   { start: new Date('2024-11-05'), end: new Date('2024-11-07') }, // trump win
 ];
 
-// Add new constant for chart colors
-const CHART_COLORS = [
-  '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF',
-  '#FF9F40', '#FF6384', '#C9CBCF', '#7BC225', '#B56DFF'
-];
-
-export { PRICE_DATA_FILE, COIN_LIST_FILE, periods, DATE, CHART_COLORS };
+export { PRICE_DATA_FILE, COIN_LIST_FILE, periods, DATE };
