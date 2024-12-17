23b2b83f09f6f7b143ab45553bcdb11f1cc82e71
0xmountaintop <haoyu@protonmail.com>
2024-12-17 16:28:36 +1100

    add new period

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,7 @@ const periods: Period[] = [
   { start: new Date('2024-08-04'), end: new Date('2024-08-06') }, // BTC dip
   { start: new Date('2024-09-06'), end: new Date('2024-09-08') }, // BTC dip
   { start: new Date('2024-11-05'), end: new Date('2024-11-07') }, // trump win
+  { start: new Date('2024-12-05'), end: new Date('2024-12-07') }, // BTC dip
 ];
 
 export { PRICE_DATA_FILE, COIN_LIST_FILE, periods, DATE };
