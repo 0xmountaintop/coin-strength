6949269b3ab212a8db7901c2bced22850126a7e9
0xmountaintop <haoyu@protonmail.com>
2025-03-07 19:20:28 +1100

    update periods

--- src/config.ts
+++ src/config.ts
@@ -13,6 +13,7 @@ const periods: Period[] = [
   { start: new Date('2024-12-19'), end: new Date('2024-12-21') }, // alt crash
   { start: new Date('2025-01-12'), end: new Date('2025-01-14') }, // BTC dip
   { start: new Date('2025-02-02'), end: new Date('2025-02-04') }, // BTC dip
+  { start: new Date('2025-02-27'), end: new Date('2025-03-01') }, // BTC dip
 ];
 
 export { PRICE_DATA_FILE, COIN_LIST_FILE, periods, DATE };
